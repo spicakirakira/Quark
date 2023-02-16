7cb0633b54d16898f2acb3aa9e405500ac74f943
MehVahdJukaar <matteogozzi1998@gmail.com>
2023-02-16 18:06:46 +0100

    blossom leaves call super on animateTick

--- src/main/java/vazkii/quark/content/world/block/BlossomLeavesBlock.java
+++ src/main/java/vazkii/quark/content/world/block/BlossomLeavesBlock.java
@@ -21,13 +21,15 @@ public class BlossomLeavesBlock extends QuarkLeavesBlock {
 
 	@Override
 	public void animateTick(@Nonnull BlockState stateIn, Level worldIn, BlockPos pos, @Nonnull RandomSource rand) {
-		if(worldIn.isEmptyBlock(pos.below()) && rand.nextInt(5) == 0 && BlossomTreesModule.dropLeafParticles) {
+		if(BlossomTreesModule.dropLeafParticles && rand.nextInt(5) == 0 && worldIn.isEmptyBlock(pos.below())) {
 			double windStrength = 5 + Math.cos((double) worldIn.getGameTime() / 2000) * 2;
 			double windX = Math.cos((double) worldIn.getGameTime() / 1200) * windStrength;
 			double windZ = Math.sin((double) worldIn.getGameTime() / 1000) * windStrength;
 
 			worldIn.addParticle(new BlockParticleOption(ParticleTypes.BLOCK, stateIn), pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, windX, -1.0, windZ);
 		}
+		//for IW
+		super.animateTick(stateIn, worldIn, pos, rand);
 	}
 
 }
