ec3e1340aceadc021a33276523929e031f9d0038
yrsegal@gmail.com <yrsegal@gmail.com>
2023-06-13 11:41:24 -0400

    sponges better

---
+++ src/main/java/vazkii/quark/mixin/SpongeBlockMixin.java
@@ -0,0 +1,17 @@
+package vazkii.quark.mixin;
+
+import net.minecraft.world.level.block.SpongeBlock;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import vazkii.quark.content.tweaks.module.SpongesBetterModule;
+
+@Mixin(SpongeBlock.class)
+public class SpongeBlockMixin {
+
+	@ModifyConstant(method = "removeWaterBreadthFirstSearch", constant = @Constant(intValue = 64))
+	public int getDrainLimit(int limit) {
+		return SpongesBetterModule.drainLimit(limit);
+	}
+
+}
