09ec67dcd71acf6189f24fa573de4c5eccc06dea
yrsegal@gmail.com <yrsegal@gmail.com>
2023-06-18 16:02:42 -0400

    failsafe for harvesting if nothing changes

--- src/main/java/vazkii/quark/mixin/HarvestFarmlandMixin.java
+++ src/main/java/vazkii/quark/mixin/HarvestFarmlandMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.ai.behavior.HarvestFarmland;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -17,7 +18,10 @@ public class HarvestFarmlandMixin {
 		at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;destroyBlock(Lnet/minecraft/core/BlockPos;ZLnet/minecraft/world/entity/Entity;)Z"))
 	private boolean harvestAndReplant(ServerLevel instance, BlockPos pos, boolean b, Entity entity) {
 		if (!SimpleHarvestModule.staticEnabled && SimpleHarvestModule.villagersUseSimpleHarvest) {
-			SimpleHarvestModule.harvestAndReplant(instance, pos, instance.getBlockState(pos), entity, ItemStack.EMPTY);
+			BlockState state = instance.getBlockState(pos);
+			SimpleHarvestModule.harvestAndReplant(instance, pos, state, entity, ItemStack.EMPTY);
+			if (state.equals(instance.getBlockState(pos)))
+				return instance.destroyBlock(pos, b, entity);
 			return true;
 		}
 		return instance.destroyBlock(pos, b, entity);
