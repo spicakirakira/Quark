c8014988ca9ec6a4ed0e5d4859c34918f8a9c077
yrsegal@gmail.com <yrsegal@gmail.com>
2023-07-05 22:06:20 -0400

    redirect is dead

--- src/main/java/vazkii/quark/mixin/client/ItemRendererMixin.java
+++ src/main/java/vazkii/quark/mixin/client/ItemRendererMixin.java
@@ -1,5 +1,6 @@
 package vazkii.quark.mixin.client;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
@@ -8,7 +9,10 @@ import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import vazkii.quark.content.management.module.ItemSharingModule;
 import vazkii.quark.content.tools.module.ColorRunesModule;
@@ -21,38 +25,38 @@ public abstract class ItemRendererMixin {
 		ColorRunesModule.setTargetStack(itemStackIn);
 	}
 
-	@Redirect(method = "getArmorFoilBuffer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderType;armorGlint()Lnet/minecraft/client/renderer/RenderType;"))
-	private static RenderType getArmorGlint() {
+	@ModifyExpressionValue(method = "getArmorFoilBuffer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderType;armorGlint()Lnet/minecraft/client/renderer/RenderType;"))
+	private static RenderType getArmorGlint(RenderType prev) {
 		return ColorRunesModule.getArmorGlint();
 	}
 
-	@Redirect(method = "getArmorFoilBuffer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderType;armorEntityGlint()Lnet/minecraft/client/renderer/RenderType;"))
-	private static RenderType getArmorEntityGlint() {
+	@ModifyExpressionValue(method = "getArmorFoilBuffer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderType;armorEntityGlint()Lnet/minecraft/client/renderer/RenderType;"))
+	private static RenderType getArmorEntityGlint(RenderType prev) {
 		return ColorRunesModule.getArmorEntityGlint();
 	}
 
-	@Redirect(method = "getFoilBuffer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderType;glintTranslucent()Lnet/minecraft/client/renderer/RenderType;"))
-	private static RenderType getGlintTranslucent() {
+	@ModifyExpressionValue(method = "getFoilBuffer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderType;glintTranslucent()Lnet/minecraft/client/renderer/RenderType;"))
+	private static RenderType getGlintTranslucent(RenderType prev) {
 		return ColorRunesModule.getGlintTranslucent();
 	}
 
-	@Redirect(method = "getFoilBuffer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderType;glint()Lnet/minecraft/client/renderer/RenderType;"))
-	private static RenderType getGlint() {
+	@ModifyExpressionValue(method = "getFoilBuffer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderType;glint()Lnet/minecraft/client/renderer/RenderType;"))
+	private static RenderType getGlint(RenderType prev) {
 		return ColorRunesModule.getGlint();
 	}
 
-	@Redirect(method = "getFoilBuffer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderType;entityGlint()Lnet/minecraft/client/renderer/RenderType;"))
-	private static RenderType getEntityGlint() {
+	@ModifyExpressionValue(method = "getFoilBuffer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderType;entityGlint()Lnet/minecraft/client/renderer/RenderType;"))
+	private static RenderType getEntityGlint(RenderType prev) {
 		return ColorRunesModule.getEntityGlint();
 	}
 
-	@Redirect(method = "getFoilBufferDirect", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderType;glintDirect()Lnet/minecraft/client/renderer/RenderType;"))
-	private static RenderType getGlintDirect() {
+	@ModifyExpressionValue(method = "getFoilBufferDirect", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderType;glintDirect()Lnet/minecraft/client/renderer/RenderType;"))
+	private static RenderType getGlintDirect(RenderType prev) {
 		return ColorRunesModule.getGlintDirect();
 	}
 
-	@Redirect(method = "getFoilBufferDirect", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderType;entityGlintDirect()Lnet/minecraft/client/renderer/RenderType;"))
-	private static RenderType getEntityGlintDirect() {
+	@ModifyExpressionValue(method = "getFoilBufferDirect", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderType;entityGlintDirect()Lnet/minecraft/client/renderer/RenderType;"))
+	private static RenderType getEntityGlintDirect(RenderType prev) {
 		return ColorRunesModule.getEntityGlintDirect();
 	}
 
