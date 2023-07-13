2baf49560ce192651dbb82e5316e4830ecb446ab
yrsegal@gmail.com <yrsegal@gmail.com>
2023-07-13 19:02:42 -0400

    apparently this was a forge method

--- src/main/java/vazkii/quark/mixin/EnchantmentMixin.java
+++ src/main/java/vazkii/quark/mixin/EnchantmentMixin.java
@@ -18,7 +18,7 @@ public class EnchantmentMixin {
 		return !EnchantmentsBegoneModule.shouldBegone(self) && (prev || canPiercingApply(self, stack));
 	}
 
-	@ModifyReturnValue(method = "canApplyAtEnchantingTable", at = @At("RETURN"))
+	@ModifyReturnValue(method = "canApplyAtEnchantingTable", at = @At("RETURN"), remap = false)
 	private boolean canApplyAtEnchantingTable(boolean prev, ItemStack stack) {
 		Enchantment self = (Enchantment) (Object) this;
 		return !EnchantmentsBegoneModule.shouldBegone(self);
