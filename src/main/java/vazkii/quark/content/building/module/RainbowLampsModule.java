60dce00279024795516cfc438df3b4aab4505da9
yrsegal@gmail.com <yrsegal@gmail.com>
2023-08-14 16:39:20 -0400

    Add hint for the lamps, and tweak a recipe

--- src/main/java/vazkii/quark/content/building/module/RainbowLampsModule.java
+++ src/main/java/vazkii/quark/content/building/module/RainbowLampsModule.java
@@ -1,12 +1,18 @@
 package vazkii.quark.content.building.module;
 
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.Block;
+import vazkii.quark.base.Quark;
 import vazkii.quark.base.module.LoadModule;
 import vazkii.quark.base.module.ModuleCategory;
-import vazkii.quark.base.module.ModuleLoader;
 import vazkii.quark.base.module.QuarkModule;
 import vazkii.quark.base.module.config.Config;
+import vazkii.quark.base.module.hint.Hint;
 import vazkii.quark.base.util.CorundumColor;
 import vazkii.quark.content.building.block.RainbowLampBlock;
+import vazkii.quark.content.world.module.CorundumModule;
 
 @LoadModule(category = ModuleCategory.BUILDING)
 public class RainbowLampsModule extends QuarkModule {
@@ -17,8 +23,17 @@ public class RainbowLampsModule extends QuarkModule {
 	@Config(description = "Whether Rainbow Lamps should be made from and themed on Corundum if that module is enabled.", flag = "rainbow_lamp_corundum")
 	public static boolean useCorundum = true;
 
+	@Hint("crystal_lamp")
+	public static TagKey<Block> lampTag;
+
 	public static boolean isCorundum() {
-		return ModuleLoader.INSTANCE.isModuleEnabled(RainbowLampsModule.class) && useCorundum;
+		return CorundumModule.staticEnabled && useCorundum;
+	}
+
+	@Override
+	public void setup() {
+		super.setup();
+		lampTag = BlockTags.create(new ResourceLocation(Quark.MOD_ID, "crystal_lamp"));
 	}
 
 	@Override
