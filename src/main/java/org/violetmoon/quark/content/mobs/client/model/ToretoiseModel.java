afe5375d5571d8a9f9a4a487403aa21894a6a50b
IThundxr <contact@ithundxr.dev>
2024-01-27 18:35:37 -0500

    brackets

--- src/main/java/org/violetmoon/quark/content/mobs/client/model/ToretoiseModel.java
+++ src/main/java/org/violetmoon/quark/content/mobs/client/model/ToretoiseModel.java
@@ -282,7 +282,7 @@ public class ToretoiseModel extends EntityModel<Toretoise> {
 
 			trans *= (1F - finalRideMultiplier);
 			rot *= (1F - finalRideMultiplier);
-			trans += finalRideMultiplier * -0.2F;
+			trans += (finalRideMultiplier * -0.2F);
 
 			matrix.pushPose();
 
