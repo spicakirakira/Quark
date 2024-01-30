c7ac9f9036cd9cf4ffabe5392504f31522b544f6
Vasco Lavos <vazkii@hotmail.com>
2024-01-30 23:07:46 +0000

    aw hell

---
+++ src/main/resources/genscripts/post_connections.py
@@ -0,0 +1,8 @@
+from jsongen import *
+
+copy([
+	('unique/block_model_post_connect.json', 'assets/{modid}/models/block/{name}_post_connect.json'),
+	('unique/block_model_post_connect_top.json', 'assets/{modid}/models/block/{name}_post_connect_top.json'),
+
+	('unique/blockstate_post.json', 'assets/{modid}/blockstates/{name}_post.json')
+])
