e5dcbbc3ed1a77f69a4aba2354bc4f58e0dccf6c
Vasco Lavos <vazkii@hotmail.com>
2024-01-30 22:37:34 +0000

    Add raw metal brick walls

--- src/main/java/org/violetmoon/quark/content/building/module/RawMetalBricksModule.java
+++ src/main/java/org/violetmoon/quark/content/building/module/RawMetalBricksModule.java
@@ -1,17 +1,17 @@
 package org.violetmoon.quark.content.building.module;
 
-import com.google.common.collect.ImmutableSet;
-
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
-
 import org.violetmoon.zeta.block.IZetaBlock;
 import org.violetmoon.zeta.block.ZetaBlock;
 import org.violetmoon.zeta.event.bus.LoadEvent;
 import org.violetmoon.zeta.event.load.ZRegister;
 import org.violetmoon.zeta.module.ZetaLoadModule;
 import org.violetmoon.zeta.module.ZetaModule;
+
+import com.google.common.collect.ImmutableSet;
+
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
 
 @ZetaLoadModule(category = "building")
 public class RawMetalBricksModule extends ZetaModule {
@@ -22,7 +22,7 @@ public class RawMetalBricksModule extends ZetaModule {
 		IZetaBlock gold = (IZetaBlock) new ZetaBlock("raw_gold_bricks", this, Properties.copy(Blocks.RAW_GOLD_BLOCK)).setCreativeTab(CreativeModeTabs.BUILDING_BLOCKS, Blocks.GOLD_BLOCK, false);
 		IZetaBlock copper = (IZetaBlock) new ZetaBlock("raw_copper_bricks", this, Properties.copy(Blocks.RAW_COPPER_BLOCK)).setCreativeTab(CreativeModeTabs.BUILDING_BLOCKS, Blocks.COPPER_BLOCK, false);
 
-		ImmutableSet.of(iron, gold, copper).forEach(what -> event.getVariantRegistry().addSlabAndStairs(what, null));
+		ImmutableSet.of(iron, gold, copper).forEach(what -> event.getVariantRegistry().addSlabStairsWall(what, null));
 	}
 
 }
